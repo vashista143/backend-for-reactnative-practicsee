bebe2c80a6282547ee14912096a6563e043d9e89
vashista <vashista.dara03@gmail.com>
2026-03-23 05:10:38 +0530

    added signina nd signup stuff

--- models/Note.js
+++ models/Note.js
@@ -6,6 +6,7 @@ const noteSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now },
   priority: { type: Number, enum: [1, 2, 3, 4, 5], required: true },
   isCompleted: { type: Boolean, required: true, default: false },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User",},
 });
 
 const Note = mongoose.model("Note", noteSchema);
--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -4,10 +4,14 @@ const {
   getAllNotes,
   deleteNote,
   editNote,
+  signin,
+  signup,
 } = require("../controllers/noteController");
 
 const router = express.Router();
 
+router.post("/signup", signup);
+router.post("/signin", signin);
 router.post("/notes", createNote);
 router.get("/notes", getAllNotes);
 router.delete("/notes/:id", deleteNote);
