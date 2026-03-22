b9e57180df5adb1a866af6428612f4d165502c34
vashista <vashista.dara03@gmail.com>
2026-03-23 05:06:56 +0530

    added user model and added signup and signin controllers

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -1,4 +1,7 @@
 const Note = require("../models/Note");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const getAllNotes = async (req, res) => {
   console.log("Fetching all notes");
@@ -74,9 +77,70 @@ const editNote = async (req, res) => {
   }
 };
 
+const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("Signup error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+
+const signin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+    const token = jwt.sign(
+      { userId: user._id },
+      "your_jwt_secret", // move to .env in real apps
+      { expiresIn: "1d" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("Signin error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 module.exports = {
   createNote,
   getAllNotes,
   editNote,
   deleteNote,
+  signup,
+  signin
 };
